178a6ae69b4ec4c6ded0328a8caede487b246775
rimi <a.fatimaarimi@gmail.com>
2023-07-20 13:54:05 -0400

    added a gmae logic that stored two cards in an array and then checks to see if the cards match

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ hardBtn.addEventListener("click", () => {
   createNewGameBoard(6, 6);
 });
 
-// clickCount is 0 because there hasn't been any clicks yet
-let clickCount = 0;
+let flippedCards = []; // Array to store and keep track of all the player's flipped cards
+let clickCount = 0; // clickCount is 0 because there hasn't been any clicks yet
 
 function updateClickCount() {
   const clickCountElement = document.getElementById("click-count");
@@ -93,16 +93,23 @@ function createNewGameBoard(rows, columns) {
     // but i'll have to attach these to the actual gameboard or else its just gonna be idk where
     gameboard.appendChild(card);
 
-    // function for card flip
-    // event listener for memory-card and make it flip on click foreach card
+    // event listener for memory-card and make it flip foreach card and apply game loop function
     card.addEventListener("click", () => {
-      card.classList.toggle("flipped");
+      // Inside the card click event listener, add the card to the flippedCards array
+      // check if the card is not flipped and there are less than 2 cards flipped
+      if (!card.classList.contains("flipped") && flippedCards.length < 2) {
+        // if conditions are met it will add the clicked cards to the flippedCard array using push
+        card.classList.add("flipped");
+        flippedCards.push(card);
 
-      // count and log every time the card is clicked
-      clickCount++;
-      console.log("Click Count: ", clickCount);
-      // inject live counter function
-      updateClickCount();
+        // once two cards are flipped we will call the gameLoop function
+        gameLoop();
+        //  increment the clickCount variable to keep track of the total number of card clicks.
+        clickCount++;
+        console.log("Click Count: ", clickCount);
+        // We call the updateClickCount() function to update the displayed click count on the webpage
+        updateClickCount();
+      }
     });
   }
 }
@@ -116,27 +123,45 @@ function randomRgbColor() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-/**
- *
- * Note to future self:
- *
- * afterwards:
- * * take a break babes, you need it **
- * - figure out gameloop
- *      - if one card is clicked
- *          - run timeOut function - startTimeout()
- *      - else check if cards match in color
- *          - clearTimeout()
- *          - check if cards match - matchCard()
- *
- * - create a startTimeout function
- *      - when first card is flipped
- *      - second card needs to be clicked in under 1.5 seconds or it will unflip
- *
- * - create a matchCard function
- *
- *
- * future:
- * - add a timeout so after and seconds counter
- *
- */
+// gameLoop is responsible for checking all of the flipped cards as well as running the game logic
+function gameLoop() {
+  // creating a constant variable that allows us to keep track of how many cards are currently flipped
+  const flippedCardCount = flippedCards.length;
+
+  // create an if else saying if the amount of flipped cards on the board is === 2
+  if (flippedCardCount === 2) {
+    // lock the gameboard so player cannot flip anymore cards
+    gameboard.style.pointerEvents = "none";
+
+    // now check if the cards match
+    // assign the first two cards from flippedCards array to variables card1 and card2 so we know the two cards we need to check
+    const [card1, card2] = flippedCards;
+
+    // selecting the front-face div elements for card 1 and card 2
+    const frontFace1 = card1.querySelector(".front-face");
+    const frontFace2 = card2.querySelector(".front-face");
+
+    // getting and storing color 1 into front face of card 1
+    const color1 = frontFace1.style.backgroundColor;
+    const color2 = frontFace2.style.backgroundColor;
+
+    // check if it matches so if color1 === color2
+    if (color1 === color2) {
+      // cards match, keep them face-up and clear the flippedCards array so player can move forward and flip two more cards
+      flippedCards = [];
+    } else {
+      // cards don't match, flip them back after a 1 second delay
+      setTimeout(() => {
+        card1.classList.remove("flipped");
+        card2.classList.remove("flipped");
+        // and clear out flipped cards 
+        flippedCards = [];
+      }, 1000);
+    }
+
+    // unlock the gameboard after so player can move forward with another move
+    setTimeout(() => {
+      gameboard.style.pointerEvents = "auto";
+    }, 1000);
+  }
+}
