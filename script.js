52fdd195deb012b4662f9f9de9828aaf9dbd5ef4
rimi <a.fatimaarimi@gmail.com>
2023-07-19 20:24:02 -0400

    creating a function to create new game board when specific mode is clicked

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
  * 
  * * Game: flipIT ****
  * 
- * 💡 My thought process: 
+ * 💡 My thought process: (and steps i took)
  * ✨ i will not create divs in HTML because DOM is my best friend ✨
  * 
  * user will have the option to click easy, medium, or hard mode
+ * when game mode is clicked it will trigger a function to createNewGameBoard
  * 
  */
 
@@ -13,16 +14,23 @@
 const easyBtn = document.getElementById('easy')
 easyBtn.addEventListener("click", () => {
     console.log("easy mode activated!");
+    createNewGameBoard();
 });
 
 // medium mode
 const mediumBtn = document.getElementById('medium')
 mediumBtn.addEventListener("click",() => {
     console.log("medium mode activated!");
+    createNewGameBoard();
 });
 
 // hard mode
 const hardBtn = document.getElementById("hard")
 hardBtn.addEventListener("click", () => {
     console.log("hard mode activated!");
+    createNewGameBoard();
 })
+
+function createNewGameBoard() {
+    
+}
