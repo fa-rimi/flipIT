04f708edbe403f5446c831e84727785e9ad77f53
rimi <a.fatimaarimi@gmail.com>
2023-07-19 20:02:35 -0400

    starting from scratch AGAIN

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * * Game: flipIT ****
+ * 
+ * 💡 My thought process: 
+ * ✨ i will not create divs in HTML because DOM is my best friend ✨
+ * 
+ * user will have the option to click easy, medium, or hard mode
+ * 
+ */
+
