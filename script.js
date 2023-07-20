7dbff52c9e13a1a8c40b1165ac922703cf296e08
rimi <a.fatimaarimi@gmail.com>
2023-07-19 20:35:37 -0400

    commented out css that i had for the first version of this game i made -- was a complete flop but the animations/styling was cute :(

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 /**
- * 
+ *
  * * Game: flipIT ****
- * 
+ *
  * 💡 My thought process: (and steps i took)
  * ✨ i will not create divs in HTML because DOM is my best friend ✨
- * 
+ *
  * user will have the option to click easy, medium, or hard mode
  * when game mode is clicked it will trigger a function to createNewGameBoard
- * 
+ * game -- will display a different amount of cards 4x4, 6x6, 8x8 =D
+ *
  */
 
 // easy mode
-const easyBtn = document.getElementById('easy')
+const easyBtn = document.getElementById("easy");
 easyBtn.addEventListener("click", () => {
-    console.log("easy mode activated!");
-    createNewGameBoard();
+  console.log("easy mode activated!");
+  // when button is clicked it will trigger a function to create a new gameboard
+
+  createNewGameBoard();
 });
 
 // medium mode
-const mediumBtn = document.getElementById('medium')
-mediumBtn.addEventListener("click",() => {
-    console.log("medium mode activated!");
-    createNewGameBoard();
+const mediumBtn = document.getElementById("medium");
+mediumBtn.addEventListener("click", () => {
+  console.log("medium mode activated!");
+  createNewGameBoard();
 });
 
 // hard mode
-const hardBtn = document.getElementById("hard")
+const hardBtn = document.getElementById("hard");
 hardBtn.addEventListener("click", () => {
-    console.log("hard mode activated!");
-    createNewGameBoard();
-})
+  console.log("hard mode activated!");
+  createNewGameBoard();
+});
 
-function createNewGameBoard() {
-    
+// new gameboard will have a certain amount of cards (rows, columns), depending on the mode that was clicked
+function createNewGameBoard(rows, columns) {
+  const totalCards = rows * columns;
 }
