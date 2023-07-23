81776eadb5aeb01d56c8d38d0ee6688169bc00b7
rimi <a.fatimaarimi@gmail.com>
2023-07-23 15:21:17 -0400

    formatTime function to convert seconds into MM:SS format

--- script.js
+++ script.js
@@ -345,3 +345,12 @@ function timeUp() {
   // implement message here -- if the timeLimitedSeconds === 0 then display message saying you failed (respectfully)
   console.log("You failed");
 }
+
+// Helper function to format time in MM:SS format
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${String(minutes).padStart(2, "0")}:${String(
+    remainingSeconds
+  ).padStart(2, "0")}`;
+}
